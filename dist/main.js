ef718910682f6df4fa9ed685608c20f89947fb96
netaniger <netanel@niger.com>
2024-12-31 17:06:16 +0200

    update workflow

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log("app is running?");
+
+
+exports.add = add;
+exports.subtract = subtract;
---
+++ dist/test.js
@@ -0,0 +1,9 @@
+const math = require('./main.js');
+
+test('add 1 + 2 to equal 3', () => {
+    expect(math.add(1,2)).toBe(3);
+});
+
+test('subtract 3 - 2 to equal 1', () => {
+    expect(math.subtract(3,2).toBe(1));
+})
