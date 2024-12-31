59bebcc7c2168b6309f40f1cc5a3dfafb692def0
netaniger <netanel@niger.com>
2024-12-31 16:14:10 +0200

    feat(main.js): aniger

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ function add(a, b) {
     return a + b;
 }
 
+function subtract(a, b) {
+    return a - b;
+}
+
 exports.add = add;
+exports.subtract = subtract;
--- src/test.js
+++ src/test.js
@@ -3,3 +3,7 @@ const math = require('./main.js');
 test('add 1 + 2 to equal 3', () => {
     expect(math.add(1,2)).toBe(3);
 });
+
+test('subtract 3 - 2 to equal 1', () => {
+    expect(math.subtract(3,2).toBe(0));
+})
